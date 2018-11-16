e5331430d823ea4c676c67ffb4bb403804798a53
Pat Underwood <pu171103@gmail.com>
2018-11-15 19:39:21 -0500

    Initial commit of Chapter 2 script.

---
+++ Chapter2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# Title: Hands-On ML, Chapter 2
+# Description: Examples from Hands-On Machine Leaning, Chapter 2
+# Author: Pat Underwood
+# Date: 11/15/2018
+
+
+
+__author__ = "Pat Underwood"
+__copyright__ = "Copyright Patrick Underwood, 2018"
+__maintainer__ = "Pat Underwood"
+__email__ = "pu171103@gmail.com"
