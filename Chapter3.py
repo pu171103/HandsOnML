cd9282069d4cb390bc52a4768b1bf35a37aa3b18
Pat Underwood <pu171103@gmail.com>
2018-11-23 10:40:55 -0500

    Initial commit of Chapter 3

---
+++ Chapter3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+# Title: Hands On ML, Chapter 3
+# Description: Examples and exercises from Chapter 3 of Hands-On Machine Learning
+# Author: Patrick Underwood
+# Date: 11/23/2018
+
