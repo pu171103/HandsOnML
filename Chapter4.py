ea70f894283c84b8323f57844ad9d86ef8c5b0bc
Pat Underwood <pu171103@gmail.com>
2018-11-24 14:27:26 -0500

    Added OLS examples to Chapter 4

--- Chapter4.py
+++ Chapter4.py
@@ -5,3 +5,37 @@
 # Author: Pat Underwood
 # Date: 11/24/2018
 
+#%%
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+#%%
+# OLS Regression
+# Generate some Gaussian noise 
+X = 2 * np.random.rand(100, 1)
+y = 4 + 3 * X + np.random.randn(100, 1)
+
+# Parameter estimation via OLS
+X_b = np.c_[np.ones((100, 1)), X]
+theta_best = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y)
+
+# Predictions with Theta hat
+X_new = np.array([[0], [2]])
+X_new_b = np.c_[np.ones((2, 1)), X_new]
+y_predict = X_new_b.dot(theta_best)
+y_predict
+
+plt.plot(X_new, y_predict, 'r-')
+plt.plot(X, y, 'b.')
+plt.axis([0, 2, 0, 15])
+plt.show()
+
+#%%
+# OLS with SciKit Learn
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+lin_reg.intercept_, lin_reg.coef_
+lin_reg.predict(X_new)
+
