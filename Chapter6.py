c03b06e67af9d187575ee7e943b584e739c3ffd8
Pat Underwood <pu171103@gmail.com>
2018-11-25 12:14:52 -0500

    Add random forest example to Chapter 6

--- Chapter6.py
+++ Chapter6.py
@@ -5,9 +5,10 @@
 # Author: Pat Underwood
 # Date: 11/25/2018
 
-#%%
+# %%
 from sklearn.datasets import load_iris
-from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.tree import (DecisionTreeClassifier, export_graphviz,
+                          DecisionTreeRegressor)
 
 # Specifying a decision tree with SKLearn
 iris = load_iris()
@@ -19,13 +20,18 @@ tree_clf.fit(X, y)
 tree_clf.predict_proba([[5, 1.5]])
 tree_clf.predict([[5, 1.5]])
 
-#%%
+# %%
 # Export tree to a format GraphViz can ingest
 export_graphviz(
-    tree_clf, 
+    tree_clf,
     out_file='iris_tree.dot',
     feature_names=iris['feature_names'][2:],
     class_names=iris['target_names'],
     rounded=True,
     filled=True
 )
+
+# %%
+# Specifying a regression tree with SciKit Learn
+tree_reg = DecisionTreeRegressor(max_depth=2)
+tree_reg.fit(X, y)
