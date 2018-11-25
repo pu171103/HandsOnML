cc86706036fbf5373c9974fbb0790df9fb1fb031
Pat Underwood <pu171103@gmail.com>
2018-11-25 07:34:46 -0500

    Add polynomial regression examples to Chapter 4

--- Chapter4.py
+++ Chapter4.py
@@ -10,6 +10,10 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression, SGDRegressor
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
 
 #%%
 # OLS Regression
@@ -83,3 +87,49 @@ for epoch in range(n_epochs):
 sgd_reg = SGDRegressor(n_iter=50, penalty=None, eta0=0.1)
 sgd_reg.fit(X, y.ravel())
 sgd_reg.intercept_, sgd_reg.coef_
+
+#%%
+# Polynomial regression
+# Generate some data
+m = 100
+X = 6 * np.random.rand(m, 1) - 3
+y = 0.5 * X**2 + X + 2 + np.random.randn(m, 1)
+
+# Generate x^2 as a separate variable
+# NOTE: By default PolynomialFeatures() calculates all possible 
+# degree combinations of the regressors you give it.
+poly_features = PolynomialFeatures(degree=2, include_bias=False)
+X_poly = poly_features.fit_transform(X) #Returns [x, x^2])
+
+# Fit the model
+lin_reg = LinearRegression()
+lin_reg.fit(X_poly, y)
+lin_reg.intercept_, lin_reg.coef_
+
+#%%
+# Plot learning curves to diagnose over/underfitting
+
+
+def plot_learning_curves(model, X, y):
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
+    train_errors, val_errors = [], []
+    
+    for m in range(1, len(X_train)):
+        model.fit(X_train[:m], y_train[:m])
+        y_train_predict = model.predict(X_train[:m])
+        y_val_predict = model.predict(X_val)
+        train_errors.append(mean_squared_error(y_train[:m], y_train_predict))
+        val_errors.append(mean_squared_error(y_val, y_val_predict))
+        plt.plot(np.sqrt(train_errors), 'r-+', linewidth=.75, label='train')
+        plt.plot(np.sqrt(val_errors), 'b-', linewidth=.75, label='val')
+
+lin_reg = LinearRegression()
+plot_learning_curves(lin_reg, X, y)
+plt.show()
+
+polynomial_regression = Pipeline([
+        ('poly_features', PolynomialFeatures(degree=10, include_bias=False)),
+        ('lin_reg', LinearRegression()),
+])
+plot_learning_curves(polynomial_regression, X, y)
+plt.show()
