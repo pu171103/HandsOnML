cb876e760d672e4164cd851bba3cda77f3c4c157
Pat Underwood <pu171103@gmail.com>
2018-11-25 11:53:26 -0500

    Add tree specification and GraphViz visualization to Chapter 6

--- Chapter6.py
+++ Chapter6.py
@@ -5,3 +5,25 @@
 # Author: Pat Underwood
 # Date: 11/25/2018
 
+#%%
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+
+# Specifying a decision tree with SKLearn
+iris = load_iris()
+X = iris['data'][:, 2:]
+y = iris['target']
+
+tree_clf = DecisionTreeClassifier(max_depth=2)
+tree_clf.fit(X, y)
+
+#%%
+# Export tree to a format GraphViz can ingest
+export_graphviz(
+    tree_clf, 
+    out_file='iris_tree.dot',
+    feature_names=iris['feature_names'][2:],
+    class_names=iris['target_names'],
+    rounded=True,
+    filled=True
+)
