8fa791f4b15d7a4165b989cc5b3334c06e8df991
Pat Underwood <pu171103@gmail.com>
2018-11-25 12:37:09 -0500

    Add hard voting ensemble example to Chapter 7

--- Chapter7.py
+++ Chapter7.py
@@ -5,3 +5,31 @@
 # Author: Pat Underwood
 # Date: 11/25/2018
 
+#%%
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.datasets import make_moons
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+#%% 
+# Specifying a voting classifier with SKLearn
+log_clf = LogisticRegression()
+rnd_clf = RandomForestClassifier()
+svm_clf = SVC()
+X, y = make_moons(1000)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+voting_clf = VotingClassifier(
+    estimators=[('lr', log_clf), ('rf', rnd_clf), ('svc', svm_clf)],
+    voting='hard'
+)
+voting_clf.fit(X_train, y_train)
+
+#%% 
+# Check each classifier's accuracy
+for clf in (log_clf, rnd_clf, svm_clf, voting_clf):
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    print(clf.__class__.__name__, accuracy_score(y_test, y_pred))
