f0913b471867833514ce1546048cf7df12a94743
Pat Underwood <pu171103@gmail.com>
2018-11-25 10:22:07 -0500

    Add basic SVM exmple to Chapter 5

--- Chapter5.py
+++ Chapter5.py
@@ -5,3 +5,26 @@
 # Author: Pat Underwood
 # Date: 11/25/2018
 
+#%%
+import numpy as np
+from sklearn import datasets
+from sklearn.datasets import make_moons
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
+from sklearn.svm import LinearSVC
+
+#%%
+# Soft margin SVM with SKLearn
+iris = datasets.load_iris()
+X = iris['data'][:, (2, 3)]
+y = (iris['target'] == 2).astype(np.float64)
+
+svm_clf = Pipeline([
+    ('Scaler', StandardScaler()),
+    ('linear_svc', LinearSVC(C=1, loss='hinge')),
+])
+svm_clf.fit(X, y)
+svm_clf.predict([[5.5, 1.7]])
+
+#%%
+# SVMs with nonlinear covariates
