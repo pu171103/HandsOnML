fd86482b49636bb19dcc033fd81d325434f68d53
Pat Underwood <pu171103@gmail.com>
2018-11-25 10:33:06 -0500

    Added SVM with nonlinear covariates example to Chapter 5

--- Chapter5.py
+++ Chapter5.py
@@ -28,3 +28,13 @@ svm_clf.predict([[5.5, 1.7]])
 
 #%%
 # SVMs with nonlinear covariates
+# SKLearn's make_moons() is a neat little function 
+# that generates useful synthetic data for clustering and classification
+polynomial_svm_clf = Pipeline([
+    ('poly_features', PolynomialFeatures(degree=3)),
+    ('scaler', StandardScaler()),
+    ('svm_clf', LinearSVC(C=10, loss='hinge'))
+])
+X, y = make_moons()
+polynomial_svm_clf.fit(X, y)
+polynomial_svm_clf.predict(X[:10, :])
