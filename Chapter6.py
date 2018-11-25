8abe48bbb7eb0ea0923e95e3bce1eb1a34b86554
Pat Underwood <pu171103@gmail.com>
2018-11-25 12:03:36 -0500

    Add prediction to basic Decision tree example in Chapter 6

--- Chapter6.py
+++ Chapter6.py
@@ -16,6 +16,8 @@ y = iris['target']
 
 tree_clf = DecisionTreeClassifier(max_depth=2)
 tree_clf.fit(X, y)
+tree_clf.predict_proba([[5, 1.5]])
+tree_clf.predict([[5, 1.5]])
 
 #%%
 # Export tree to a format GraphViz can ingest
