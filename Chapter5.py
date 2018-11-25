93572065bb48c785cd029c227f8b24c8958124d2
Pat Underwood <pu171103@gmail.com>
2018-11-25 11:06:59 -0500

    Add support vector regression example to Chapter 5

--- Chapter5.py
+++ Chapter5.py
@@ -7,11 +7,12 @@
 
 #%%
 import numpy as np
+from numpy.random import randint
 from sklearn import datasets
 from sklearn.datasets import make_moons
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, PolynomialFeatures
-from sklearn.svm import LinearSVC, SVC
+from sklearn.svm import LinearSVC, SVC, LinearSVR, SVR
 
 #%%
 # Soft margin SVM with SKLearn
@@ -45,4 +46,28 @@ poly_kernel_svm_clf = Pipeline([
     ('scaler', StandardScaler()),
     ('svm_clf', SVC(kernel='poly', degree=3, coef0=1, C=5))
 ])
+X, y = make_moons()
+poly_kernel_svm_clf.fit(X, y)
 
+#%% 
+# The 'landmark trick,' via the kernel trick
+# The Gaussian Radial Basis Function provides a metric of distance of a 
+# data point from some reference point in the parameter space.
+# We will use that as a mapping function with the kernel trick.
+rbf_kernel_svm_clf = Pipeline([
+    ('scaler', StandardScaler()),
+    ('svm_clf', SVC(kernel='rbf', gamma=5, C=0.001))
+])
+rbf_kernel_svm_clf.fit(X, y)
+
+#%%
+# Support Vector Regression
+# Sort of the 'opposite' of SVM- find a margin that contains most data points
+X = randint(0, 100, 10000).reshape(-1, 1)
+y = 25 + X**2
+svm_reg = LinearSVR(epsilon=1.5)
+svm_reg.fit(X, y)
+svm_reg.predict(X[:10, :])
+
+svm_poly_reg = SVR(kernel='poly', degree=2, C=100, epsilon=0.1)
+svm_poly_reg.fit(X, y)
